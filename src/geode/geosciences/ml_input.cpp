b96eeaf98217728d4eeadb371fbe3234edc66008
Arnaud Botella <arnaud.botella@geode-solutions.com>
2019-09-22 06:11:19 +0200

    Update ml_input.cpp

--- src/geode/geosciences/ml_input.cpp
+++ src/geode/geosciences/ml_input.cpp
@@ -482,7 +482,7 @@ namespace
                 else if( tsurf.feature == "boundary"
                          || tsurf.feature == "lease" )
                 {
-                    // todo ahndle ModelBoundary
+                    // todo handle ModelBoundary
                 }
                 else
                 {
@@ -551,7 +551,7 @@ namespace
                 std::string boundary_line;
                 std::getline( file_, boundary_line );
                 std::istringstream boundary_iss{ boundary_line };
-                int surface_id{ 0 };
+                int surface_id;
                 while( boundary_iss >> surface_id )
                 {
                     if( surface_id == 0 )
