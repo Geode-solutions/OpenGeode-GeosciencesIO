5e4a431261556121c9985bf41dde628c60cccbbd
MelchiorSchuh <melchior.schuh-senlis@geode-solutions.com>
2023-06-29 11:21:22 +0200

    fix(vo_input): Fixed compilation with new grid initialization coming with grid directions.

--- src/geode/geosciences_io/mesh/vo_input.cpp
+++ src/geode/geosciences_io/mesh/vo_input.cpp
@@ -30,7 +30,7 @@
 #include <geode/basic/string.h>
 
 #include <geode/geometry/distance.h>
-#include <geode/geometry/point.h>
+#include <geode/geometry/vector.h>
 
 #include <geode/mesh/builder/regular_grid_solid_builder.h>
 #include <geode/mesh/core/regular_grid_solid.h>
