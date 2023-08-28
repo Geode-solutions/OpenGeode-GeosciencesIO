fe1c86e4fddbd0f9669b290e3377310332b0d814
MelchiorSchuh <melchior.schuh-senlis@geode-solutions.com>
2023-08-28 17:18:16 +0200

    feat(TSOutput): Added the output of TriangulatedSurface3D to .ts format.

---
+++ src/geode/geosciences_io/mesh/ts_output.cpp
@@ -0,0 +1,193 @@
+/*
+ * Copyright (c) 2019 - 2023 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <geode/geosciences_io/mesh/private/ts_output.h>
+
+#include <fstream>
+#include <memory>
+#include <vector>
+
+#include <geode/basic/attribute_manager.h>
+#include <geode/basic/types.h>
+
+#include <geode/basic/filename.h>
+#include <geode/mesh/core/triangulated_surface.h>
+
+#include <geode/geosciences_io/mesh/private/gocad_common.h>
+
+namespace
+{
+    class TSOutputImpl
+    {
+    public:
+        static constexpr geode::index_t OFFSET_START{ 1 };
+        static constexpr char EOL{ '\n' };
+        static constexpr char SPACE{ ' ' };
+        TSOutputImpl( absl::string_view filename,
+            const geode::TriangulatedSurface3D& surface )
+            : file_{ geode::to_string( filename ) },
+              surface_( surface ),
+              triangle_done_( surface.nb_polygons(), false )
+        {
+            OPENGEODE_EXCEPTION(
+                file_.good(), "Error while opening file: ", filename );
+        }
+
+        void write_file()
+        {
+            geode::Logger::info( "[TSOutput::write] Writing ts file." );
+            file_ << "GOCAD TSurf 1" << EOL;
+            geode::detail::HeaderData header;
+            header.name = geode::to_string( surface_.name() );
+            geode::detail::write_header( file_, header );
+            geode::detail::write_CRS( file_, {} );
+            write_prop_header();
+            write_tface();
+            file_ << "END" << EOL;
+            return;
+        }
+
+        void write_prop_header()
+        {
+            const auto names =
+                surface_.vertex_attribute_manager().attribute_names();
+            geode::detail::PropHeaderData prop_header;
+            std::vector< geode::detail::PropClassHeaderData >
+                header_properties_data;
+            header_properties_data.reserve( names.size() );
+
+            for( const auto& name : names )
+            {
+                const auto attribute =
+                    surface_.vertex_attribute_manager().find_generic_attribute(
+                        name );
+                if( !attribute || !attribute->is_genericable()
+                    || name == "points" )
+                {
+                    continue;
+                }
+                generic_att_.push_back( attribute );
+                prop_header.names.emplace_back( geode::to_string( name ) );
+                prop_header.prop_legal_ranges.push_back(
+                    std::make_pair( "**none**", "**none**" ) );
+                prop_header.no_data_values.push_back( -99999. );
+                prop_header.property_classes.emplace_back(
+                    geode::to_string( name ) );
+                prop_header.kinds.push_back( "Real Number" );
+                prop_header.property_subclass.push_back(
+                    std::make_pair( "QUANTITY", "Float" ) );
+                prop_header.esizes.push_back( 1 );
+                prop_header.units.push_back( "unitless" );
+
+                geode::detail::PropClassHeaderData tmp_prop_class_header;
+                tmp_prop_class_header.name = geode::to_string( name );
+
+                header_properties_data.push_back( tmp_prop_class_header );
+            }
+            if( !prop_header.empty() )
+            {
+                geode::detail::write_prop_header( file_, prop_header );
+            }
+            write_XYZ_prop_class_header();
+            for( const auto& property_data : header_properties_data )
+            {
+                write_property_class_header( file_, property_data );
+            }
+        }
+
+        void write_XYZ_prop_class_header()
+        {
+            geode::detail::PropClassHeaderData x_prop_header;
+            x_prop_header.name = "X";
+            x_prop_header.kind = "X";
+            x_prop_header.unit = "m";
+            x_prop_header.is_z = false;
+            write_property_class_header( file_, x_prop_header );
+
+            geode::detail::PropClassHeaderData y_prop_header;
+            y_prop_header.name = "Y";
+            y_prop_header.kind = "Y";
+            y_prop_header.unit = "m";
+            y_prop_header.is_z = false;
+            write_property_class_header( file_, y_prop_header );
+
+            geode::detail::PropClassHeaderData z_prop_header;
+            z_prop_header.name = "Z";
+            z_prop_header.kind = "Z";
+            z_prop_header.unit = "m";
+            z_prop_header.is_z = true;
+            write_property_class_header( file_, z_prop_header );
+        }
+
+        void write_tface()
+        {
+            file_ << "TFACE" << EOL;
+            for( const auto v : geode::Range{ surface_.nb_vertices() } )
+            {
+                write_vrtx( v );
+            }
+            for( const auto triangle_id :
+                geode::Range{ surface_.nb_polygons() } )
+            {
+                write_triangle( triangle_id );
+            }
+        }
+
+        void write_vrtx( const geode::index_t vertex_id )
+        {
+            file_ << "VRTX" << SPACE << vertex_id << SPACE
+                  << surface_.point( vertex_id ).string();
+            for( const auto& att : generic_att_ )
+            {
+                file_ << SPACE << att->generic_value( vertex_id );
+            }
+            file_ << EOL;
+        }
+
+        void write_triangle( const geode::index_t triangle_id )
+        {
+            const auto& vertices = surface_.polygon_vertices( triangle_id );
+            file_ << "TRGL" << SPACE << vertices[0] << SPACE << vertices[1]
+                  << SPACE << vertices[2];
+            file_ << EOL;
+        }
+
+    private:
+        std::ofstream file_;
+        const geode::TriangulatedSurface3D& surface_;
+        std::vector< std::shared_ptr< geode::AttributeBase > > generic_att_;
+        std::vector< bool > triangle_done_;
+    };
+} // namespace
+
+namespace geode
+{
+    namespace detail
+    {
+        void TSOutput::write( const TriangulatedSurface3D& surface ) const
+        {
+            TSOutputImpl impl{ filename(), surface };
+            impl.write_file();
+        }
+    } // namespace detail
+} // namespace geode
