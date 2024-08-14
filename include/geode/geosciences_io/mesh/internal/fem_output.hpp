abf4f5fed1ebb4983244055d600df74d53d47019
benoitTHEBAULT <benoit.thebault@geode-solutions.com>
2024-08-14 16:47:01 +0200

    feat(GeosciencesIO): adding Solid and BRep fem writer

---
+++ include/geode/geosciences_io/mesh/internal/fem_output.hpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019 - 2024 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+#include <string>
+#include <vector>
+
+#include <geode/mesh/core/tetrahedral_solid.hpp>
+
+#include <geode/mesh/io/tetrahedral_solid_output.hpp>
+
+#include <geode/geosciences_io/mesh/common.hpp>
+
+#pragma once
+
+namespace geode
+{
+    FORWARD_DECLARATION_DIMENSION_CLASS( TetrahedralSolid );
+    ALIAS_3D( TetrahedralSolid );
+} // namespace geode
+
+namespace geode
+{
+    namespace internal
+    {
+        class SolidFemOutput : public TetrahedralSolidOutput< 3 >
+        {
+        public:
+            explicit SolidFemOutput( std::string_view filename )
+                : TetrahedralSolidOutput< 3 >( filename )
+            {
+            }
+
+            static std::string_view extension()
+            {
+                static constexpr auto EXT = "fem";
+                return EXT;
+            }
+
+            std::vector< std::string > write(
+                const TetrahedralSolid3D& solid ) const;
+            std::vector< std::string > write() const;
+        };
+    } // namespace internal
+} // namespace geode
