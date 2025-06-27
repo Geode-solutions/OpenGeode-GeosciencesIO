0823e40fa2dbaf55ea4d5dcf8be7e9b58a8661cc
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-06-27 23:10:36 +0200

    feat(DEM): add input support for dem files

---
+++ tests/mesh/test-dem.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 - 2025 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <geode/tests_config.hpp>
+
+#include <geode/basic/assert.hpp>
+#include <geode/basic/logger.hpp>
+
+#include <geode/mesh/core/polygonal_surface.hpp>
+
+#include <geode/mesh/io/polygonal_surface_input.hpp>
+#include <geode/mesh/io/polygonal_surface_output.hpp>
+
+#include <geode/geosciences_io/mesh/common.hpp>
+
+int main()
+{
+    try
+    {
+        geode::GeosciencesIOMeshLibrary::initialize();
+        geode::Logger::set_level( geode::Logger::LEVEL::trace );
+        const auto surface = geode::load_polygonal_surface< 3 >(
+            absl::StrCat( geode::DATA_PATH, "bathy_IrishSea_DEM.dem" ) );
+        OPENGEODE_EXCEPTION( surface->nb_vertices() == 456427,
+            "[Test] Number of vertices in the loaded Surface is not correct" );
+        OPENGEODE_EXCEPTION( surface->nb_polygons() == 453226,
+            "[Test] Number of polygons in the loaded Surface is not correct" );
+        geode::save_polygonal_surface(
+            *surface, "bathy_IrishSea_DEM.og_psf3d" );
+
+        geode::Logger::info( "[TEST SUCCESS]" );
+
+        return 0;
+    }
+    catch( ... )
+    {
+        return geode::geode_lippincott();
+    }
+}
